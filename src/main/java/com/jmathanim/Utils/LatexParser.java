c72be13f4351b37c73d0432306236ac01fa61afa
David Gutiérrez <davidgutierrezrubio@gmail.com>
2024-04-11 18:45:15 +0200

    -Added first version of LatexParser to identify latex tokens using jLatexMath library

--- src/main/java/com/jmathanim/Utils/LatexParser.java
+++ src/main/java/com/jmathanim/Utils/LatexParser.java
@@ -24,26 +24,35 @@ import com.jmathanim.jmathanim.JMathAnimScene;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.scilab.forge.jlatexmath.ArrayOfAtoms;
 import org.scilab.forge.jlatexmath.Atom;
 import org.scilab.forge.jlatexmath.BigOperatorAtom;
 import org.scilab.forge.jlatexmath.CharAtom;
 import org.scilab.forge.jlatexmath.EmptyAtom;
 import org.scilab.forge.jlatexmath.FencedAtom;
 import org.scilab.forge.jlatexmath.FractionAtom;
+import org.scilab.forge.jlatexmath.MatrixAtom;
+import org.scilab.forge.jlatexmath.MiddleAtom;
 import org.scilab.forge.jlatexmath.NthRoot;
 import org.scilab.forge.jlatexmath.RomanAtom;
 import org.scilab.forge.jlatexmath.RowAtom;
 import org.scilab.forge.jlatexmath.ScriptsAtom;
 import org.scilab.forge.jlatexmath.SpaceAtom;
 import org.scilab.forge.jlatexmath.SymbolAtom;
+import org.scilab.forge.jlatexmath.TeXConstants;
 import org.scilab.forge.jlatexmath.TeXFormula;
+import org.scilab.forge.jlatexmath.TeXIcon;
+import org.scilab.forge.jlatexmath.TeXParser;
 import org.scilab.forge.jlatexmath.TypedAtom;
 
 public class LatexParser {
+
+    public List<MiddleAtom> list;
 
     public final ArrayList<LatexToken> tokens;
 
@@ -53,6 +62,7 @@ public class LatexParser {
     public Modifier modifier;
 
     public LatexParser() {
+        this.list=new ArrayList<MiddleAtom>();
         this.tokens = new ArrayList<>();
         this.modifier = Modifier.NORMAL;
     }
@@ -60,6 +70,25 @@ public class LatexParser {
     public void parse(String texto) {
         this.tokens.clear();
         TeXFormula formula = new TeXFormula(texto);
+        TeXIcon icon = formula.createTeXIcon(TeXConstants.ALIGN_LEFT, 40);
+        Field campo;
+        try {
+            campo = TeXFormula.class.getDeclaredField("parser");
+            campo.setAccessible(true);
+            TeXParser texParser = (TeXParser) campo.get(formula);
+            texParser.parse();
+            TeXParser tp2=new TeXParser(texto, formula, false);
+            tp2.parse();
+        } catch (NoSuchFieldException ex) {
+            Logger.getLogger(LatexParser.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SecurityException ex) {
+            Logger.getLogger(LatexParser.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalArgumentException ex) {
+            Logger.getLogger(LatexParser.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(LatexParser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
         Atom root = formula.root;
         try {
             parseAtom(root);
@@ -124,6 +153,14 @@ public class LatexParser {
             campo = FencedAtom.class.getDeclaredField("right");
             campo.setAccessible(true);
             parseAtom(campo.get(fencedAtom));
+
+            campo = FencedAtom.class.getDeclaredField("middle");
+            campo.setAccessible(true);
+            list = (List<MiddleAtom>) campo.get(fencedAtom);
+            for (MiddleAtom middleAtom : list) {
+                parseAtom(middleAtom);
+
+            }
         }
 
         if (atom instanceof NthRoot) {
@@ -199,6 +236,20 @@ public class LatexParser {
             campo = BigOperatorAtom.class.getDeclaredField("under");
             campo.setAccessible(true);
             parseAtom((Atom) campo.get(bigOperatorAtom));
+        }
+        if (atom instanceof MatrixAtom) {
+            MatrixAtom matrixAtom = (MatrixAtom) atom;
+            campo = MatrixAtom.class.getDeclaredField("matrix");
+            campo.setAccessible(true);
+            ArrayOfAtoms arrayofAtoms = (ArrayOfAtoms) campo.get(matrixAtom);
+            LinkedList<LinkedList<Atom>> array = arrayofAtoms.array;
+            for (LinkedList<Atom> linkedList : array) {
+                for (Atom atom1 : linkedList) {
+                    parseAtom(atom1);
+
+                }
+
+            }
         }
 
     }
