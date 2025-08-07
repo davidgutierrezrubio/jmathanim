94fdb879217c81494147f7776794ef5fb99f3283
David Gutiérrez <davidgutierrezrubio@gmail.com>
2025-08-07 10:37:01 +0200

    -Preliminary version of TwistTransform

--- src/main/java/com/jmathanim/Animations/Strategies/Transform/TwistTransform.java
+++ src/main/java/com/jmathanim/Animations/Strategies/Transform/TwistTransform.java
@@ -2,31 +2,53 @@ package com.jmathanim.Animations.Strategies.Transform;
 
 import com.jmathanim.Utils.Vec;
 import com.jmathanim.mathobjects.MathObjectGroup;
+import com.jmathanim.mathobjects.Point;
 import com.jmathanim.mathobjects.Shape;
-import com.jmathanim.mathobjects.updateableObjects.Trail;
 
-import java.util.ArrayList;
+import java.util.function.DoubleUnaryOperator;
 
 public class TwistTransform extends TransformShape2ShapeStrategy {
 
-    private int numPoint;
+    private int numPivotalSegment;
     private Vec vShift;
     private double[] originAngles;
     private double[] destinyAngles;
-    private double[] originLengths;
-    private double[] destinyLengths;
+    private double[] ratioLengths;
+    private double pivotalAngle;
+    private double pivotalLengthRatio;
+    private DoubleUnaryOperator lambdaForward;
+
+    public DoubleUnaryOperator getLambdaBackward() {
+        return lambdaBackward;
+    }
+
+    public void setLambdaBackward(DoubleUnaryOperator lambdaBackward) {
+        this.lambdaBackward = lambdaBackward;
+    }
+
+    public DoubleUnaryOperator getLambdaForward() {
+        return lambdaForward;
+    }
+
+    public void setLambdaForward(DoubleUnaryOperator lambdaForward) {
+        this.lambdaForward = lambdaForward;
+    }
+
+    private DoubleUnaryOperator lambdaBackward;
 
     public TwistTransform(double runTime, Shape origin, Shape destiny, int numPoint) {
         super(runTime);
         setOrigin(origin);
         setDestiny(destiny);
-        numPoint = numPoint;
+        numPivotalSegment = numPoint;
+        lambdaBackward=null;
+        lambdaForward=null;
     }
 
     @Override
     public boolean doInitialization() {
         super.doInitialization();
-        vShift = getOriginObject().get(numPoint).p.to(getDestinyObject().get(numPoint).p);
+        vShift = getOriginObject().get(numPivotalSegment).p.to(getDestinyObject().get(numPivotalSegment).p);
         setIntermediate(getOriginObject().copy());
         getIntermediateObject().drawColor("red");
         getIntermediateObject().setObjectLabel("intermediate");
@@ -34,41 +56,42 @@ public class TwistTransform extends TransformShape2ShapeStrategy {
         getDestinyObject().setObjectLabel("destiny");
         saveStates(getIntermediateObject());
 
+        //DEBUG, to delete
         for (int i = 0; i < getIntermediateObject().size(); i++) {
-//            scene.add(Trail.make(getIntermediateObject().get(i).p).drawColor("blue"));
+            scene.add((getIntermediateObject().get(i).p).drawColor("blue"));
         }
 
         //Calcular angulos
         int size = getOriginObject().size();
-        originAngles=new double[size-1];
-        destinyAngles=new double[size-1];
-        originLengths=new double[size-1];
-        destinyLengths=new double[size-1];
+        originAngles = new double[size];
+        destinyAngles = new double[size];
+        ratioLengths = new double[size - 1];
 
-        double[] tempOrigAngles=new double[size-1];
-        double[] tempDestinyAngles=new double[size-1];
+        double[] tempOrigAngles = new double[size];
+        double[] tempDestinyAngles = new double[size];
 
 
         for (int i = 0; i < size - 1; i++) {
             Vec v1 = getOriginObject().get(i).p.to(getOriginObject().get(i + 1).p);
-            tempOrigAngles[i]= v1.getAngle();
-            originLengths[i]=v1.norm();
+            tempOrigAngles[i] = v1.getAngle();
             Vec v2 = getDestinyObject().get(i).p.to(getDestinyObject().get(i + 1).p);
-            tempDestinyAngles[i]= v2.getAngle();
-            destinyLengths[i] = v2.norm();
-        }
-        originAngles[0]=tempOrigAngles[0];
-        destinyAngles[0]=tempDestinyAngles[0];
-
-        for (int i=1; i<size-1;i++) {
-            originAngles[i]=normalizeAngle(tempOrigAngles[i]-tempOrigAngles[i-1]+Math.PI);
-            destinyAngles[i]=normalizeAngle(tempDestinyAngles[i]-tempDestinyAngles[i-1]+Math.PI);
+            tempDestinyAngles[i] = v2.getAngle();
+            ratioLengths[i] = v2.norm() / v1.norm() - 1;
         }
 
+
+        for (int i = 1; i < size - 1; i++) {
+            originAngles[i] = normalizeAngle(tempOrigAngles[i] - tempOrigAngles[i - 1] + Math.PI);
+            destinyAngles[i] = normalizeAngle(tempDestinyAngles[i] - tempDestinyAngles[i - 1] + Math.PI);
+        }
+
+        pivotalLengthRatio = ratioLengths[numPivotalSegment];
+        pivotalAngle = tempDestinyAngles[numPivotalSegment] - tempOrigAngles[numPivotalSegment];
 
 
         return true;
     }
+
     private double normalizeAngle(double angle) {
         double twoPi = 2 * Math.PI;
         angle = angle % twoPi;
@@ -79,29 +102,27 @@ public class TwistTransform extends TransformShape2ShapeStrategy {
     }
 
 
-
     @Override
     public void doAnim(double t) {
         super.doAnim(t);
         double lt = getLT(t);
+        double rt;
+        rt = (t < 0 ? 0 : t);
+        rt = (rt > 1 ? 1 : rt);
+
+        double ltf = (lambdaForward == null ? lt : lambdaForward.applyAsDouble(rt));
+        double ltb = (lambdaBackward == null ? lt : lambdaBackward.applyAsDouble(rt));
+
+
+
         Shape intermediateObject = getIntermediateObject();
         restoreStates(intermediateObject);
-        intermediateObject.shift(vShift.mult(lt));
 
-        int size = getOriginObject().size();
-        for (int i = 0; i < size-1; i++) {
 
-            MathObjectGroup mg = MathObjectGroup.make();
-            for (int j = i+1; j < size; j++) {
-                mg.add(getIntermediateObject().get(j));
-            }
-
-            double ang=destinyAngles[i]-originAngles[i];
-            double lambda1 = destinyLengths[i]/originLengths[i] - 1;
-            Vec v=getIntermediateObject().get(i).p.to(getIntermediateObject().get(i+1).p);
-            mg.shift(v.mult(lambda1));
-            mg.rotate(getIntermediateObject().get(i).p,ang*lt);
-        }
+        int numPoint = 1;
+        applyTransformToPivotalSegment(numPivotalSegment, lt);
+        applyTransformForwardPoints(numPivotalSegment, ltf);
+        applyTransformBackwardPoints(numPivotalSegment, ltb);
 
         if (isShouldInterpolateStyles()) {
             intermediateObject.getMp().interpolateFrom(getOriginObject().getMp(), getDestinyObject().getMp(), lt);
@@ -110,4 +131,53 @@ public class TwistTransform extends TransformShape2ShapeStrategy {
         applyAnimationEffects(lt, intermediateObject);
 
     }
+
+    private MathObjectGroup subPath(int pivotal, boolean forward) {
+        int size = getIntermediateObject().size();
+        MathObjectGroup mg = MathObjectGroup.make();
+        if (forward)
+            for (int j = pivotal + 1; j < size; j++) {
+                mg.add(getIntermediateObject().get(j));
+            }
+        else {
+            for (int j = pivotal - 1; j >=0; j--) {
+                mg.add(getIntermediateObject().get(j));
+            }
+        }
+        return mg;
+    }
+
+    private void applyTransformToPivotalSegment(int numPivotalSegment, double lt) {
+        getIntermediateObject().shift(vShift.mult(lt));
+        Point pivotPoint = getIntermediateObject().get(numPivotalSegment).p;
+        getIntermediateObject().rotate(pivotPoint, pivotalAngle * lt);
+        MathObjectGroup mg=subPath(numPivotalSegment,true);
+        Vec v = pivotPoint.to(getIntermediateObject().get(numPivotalSegment+1).p);
+        mg.shift(v.mult(pivotalLengthRatio*lt));
+    }
+
+    private void applyTransformForwardPoints(int numPivotalSegment, double lt) {
+        int size = getOriginObject().size();
+        if (numPivotalSegment==size-1) return; //No forward points
+        for (int i = numPivotalSegment+1; i < size - 1; i++) {
+            MathObjectGroup mg=subPath(i,true);
+            double ang = destinyAngles[i] - originAngles[i];
+            Vec v = getIntermediateObject().get(i).p.to(getIntermediateObject().get(i + 1).p);
+            mg.shift(v.mult(ratioLengths[i]));
+            mg.rotate(getIntermediateObject().get(i).p, ang * lt);
+        }
+    }
+
+    private void applyTransformBackwardPoints(int numPivotalSegment, double lt) {
+        if (numPivotalSegment==0) return;//No backward points
+        for (int i = numPivotalSegment; i > 0; i--) {
+            MathObjectGroup mg=subPath(i,false);
+            double ang = destinyAngles[i] - originAngles[i];
+            Vec v = getIntermediateObject().get(i).p.to(getIntermediateObject().get(i-1).p);
+            mg.shift(v.mult(ratioLengths[i-1]*lt));
+            mg.rotate(getIntermediateObject().get(i).p, -ang * lt);
+        }
+    }
+
+
 }
