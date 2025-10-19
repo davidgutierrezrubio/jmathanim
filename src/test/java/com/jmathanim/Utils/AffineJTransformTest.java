141094ef5d8a80e38bd1c69fee8de7fda6b2316b
DavidGutierrezRubio <davidgutierrezrubio@gmail.com>
2025-10-19 13:55:52 +0200

    -Fixed several bugs in Geogebra import
    Signed-off-by: DavidGutierrezRubio <davidgutierrezrubio@gmail.com>

--- src/test/java/com/jmathanim/Utils/AffineJTransformTest.java
+++ src/test/java/com/jmathanim/Utils/AffineJTransformTest.java
@@ -9,7 +9,7 @@ public class AffineJTransformTest {
 
     @Test
     public void testEquals() {
-        AffineJTransform tr1 = AffineJTransform.create2DRotationTransform(Vec.to(0, 0),PI/4);
+        AffineJTransform tr1 = AffineJTransform.create2DRotationTransform(Vec.to(1, 0),PI/4);
         AffineJTransform tr2 = AffineJTransform.create2DRotationTransform(Vec.to(1, 0),PI/4);
         assertAffineJTransformEquals(tr1, tr2," different matrices");
 
