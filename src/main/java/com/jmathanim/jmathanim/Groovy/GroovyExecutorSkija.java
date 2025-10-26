43fd1d5fe225c6e72e0be8593056e3c5e7139a31
DavidGutierrezRubio <davidgutierrezrubio@gmail.com>
2025-10-26 16:33:47 +0100

    -Added skija renderer test for groovy executor
    Signed-off-by: DavidGutierrezRubio <davidgutierrezrubio@gmail.com>

--- src/main/java/com/jmathanim/jmathanim/Groovy/GroovyExecutorSkija.java
+++ src/main/java/com/jmathanim/jmathanim/Groovy/GroovyExecutorSkija.java
@@ -2,14 +2,14 @@ package com.jmathanim.jmathanim.Groovy;
 
 import com.jmathanim.jmathanim.JMathAnimScene;
 import com.jmathanim.jmathanim.LogUtils;
-import com.jmathanim.jmathanim.Scene2D;
+import com.jmathanim.jmathanim.Scene2DSkija;
 import javafx.application.Platform;
 
 import java.io.File;
 
 import static com.jmathanim.jmathanim.LogUtils.RESET;
 
-public class GroovyExecutor extends Scene2D {
+public class GroovyExecutorSkija extends Scene2DSkija {
 
 
     private final GroovyUtils groovyUtils;
@@ -19,7 +19,7 @@ public class GroovyExecutor extends Scene2D {
      *
      * @param groovyScriptFileName Filename of Groovy Script filename
      */
-    public GroovyExecutor(String groovyScriptFileName) {
+    public GroovyExecutorSkija(String groovyScriptFileName) {
         super();
         groovyUtils = new GroovyUtils(groovyScriptFileName, this);
 
@@ -41,7 +41,7 @@ public class GroovyExecutor extends Scene2D {
             File file = new File(arg);
             String absPath = file.getAbsolutePath();
             logger.info("Running Groovy Script " + LogUtils.BLUE + absPath + RESET);
-            JMathAnimScene tr = new GroovyExecutor(absPath);
+            JMathAnimScene tr = new GroovyExecutorSkija(absPath);
             tr.getConfig().setScriptMode(true);
             tr.execute();
             if (tr.getConfig().isJavaFXRunning()) {
--- src/main/java/com/jmathanim/jmathanim/Groovy/GroovyUtils.java
+++ src/main/java/com/jmathanim/jmathanim/Groovy/GroovyUtils.java
@@ -1,13 +1,112 @@
 package com.jmathanim.jmathanim.Groovy;
 
+import com.jmathanim.jmathanim.JMathAnimScene;
 import com.jmathanim.jmathanim.LogUtils;
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+import groovy.lang.Script;
+import org.codehaus.groovy.control.CompilerConfiguration;
+import org.codehaus.groovy.control.customizers.ImportCustomizer;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import java.util.stream.Collectors;
 
-import static com.jmathanim.jmathanim.JMathAnimScene.logger;
+import static com.jmathanim.jmathanim.JMathAnimScene.*;
 import static com.jmathanim.jmathanim.LogUtils.RESET;
 
 public class GroovyUtils {
+    private final JMathAnimScene scene;
+    public String userCodeOriginal;
+    public String userCode;
+    private Script script = null;
+
+    private final String groovyScriptFilename;
+
+    public GroovyUtils(String groovyScriptFilename,JMathAnimScene scene) {
+        this.groovyScriptFilename = groovyScriptFilename;
+        this.scene=scene;
+        Binding binding = GroovyUtils.createBindings(scene);
+        CompilerConfiguration compilerConfig = GroovyUtils.createCompilerConfiguration(scene);
+
+        GroovyShell shell = new GroovyShell(scene.getClass().getClassLoader(), binding, compilerConfig);
+
+        try {
+            userCodeOriginal = new String(Files.readAllBytes(Paths.get(groovyScriptFilename)));
+            userCode = GroovyUtils.processSourceCode(userCodeOriginal);
+            script = shell.parse(userCode);
+
+        } catch (IOException e) {
+            logger.error("File not found: " + groovyScriptFilename);
+            System.out.println(e.getLocalizedMessage());
+            e.printStackTrace();
+
+            System.exit(1);
+        }
+    }
+
+    public void runSetup() {
+
+        boolean hasSetupSketch = false;
+        for (Method method : script.getClass().getMethods()) {
+            if (method.getName().equals("setupSketch") && method.getParameterCount() == 0) {
+                hasSetupSketch = true;
+                break;
+            }
+        }
+        File file = new File(groovyScriptFilename);
+        String fileNameGroovy = file.getName();
+        String fileNameGroovyWithoutExtension = fileNameGroovy.substring(0, fileNameGroovy.lastIndexOf("."));
+        scene.config.setOutputFileName(fileNameGroovyWithoutExtension);
+        if (!hasSetupSketch) {//no init
+//            logger.setLevel(Level.DEBUG);
+//            logger.info("No init() method, switching to default configuration (preview, dark style)");
+//            scene.config.parseFile("#preview.xml");
+//            scene.config.parseFile("#dark.xml");
+//            scene.config.setLimitFPS(true);
+////            config.setDefaultLambda(t -> t);
+        } else {
+            try {
+                script.invokeMethod("setupSketch", null);
+            } catch (Exception e) {
+                System.out.println("Error parsing config script");
+                System.exit(1);
+            }
+
+        }
+    }
+
+    public void runSketch() {
+        boolean hasRunSketch = false;
+        try {
+            for (Method method : script.getClass().getMethods()) {
+                if (method.getName().equals("runSketch") && method.getParameterCount() == 0) {
+                    hasRunSketch = true;
+                    break;
+                }
+            }
+
+            if (hasRunSketch) {
+                script.invokeMethod("runSketch", null);
+            } else {//If script does not have runSketch method, just run root code
+                script.run();
+            }
+
+        } catch (Exception e) {
+            logger.error(LogUtils.RED + "Error running Groovy Script" + RESET);
+//    GroovyExceptionInfo.processGroovyError(e,scriptText);
+            GroovyUtils.processGroovyError(e, userCode, userCodeOriginal);
+        }
+    }
 
     // Métodos auxiliares
     public static String addLineNumbers(String script) {
@@ -52,4 +151,150 @@ public class GroovyUtils {
                 .filter(line -> !line.contains("@Field") && !line.contains("import groovy.transform.Field"))
                 .collect(Collectors.joining("\n"));
     }
+
+    public static Binding createBindings(JMathAnimScene scene) {
+        Binding binding = new Binding();
+        binding.setVariable("scene", scene);
+
+        binding.setVariable("PI", PI);
+        binding.setVariable("DEGREES", DEGREES);
+
+        binding.setVariable("play", scene.play);
+        binding.setVariable("config", scene.config);
+
+        binding.setVariable("camera", scene.getCamera());
+        binding.setVariable("fixedCamera", scene.getFixedCamera());
+
+        //Creates Closure for add method
+//        Closure<Void> addClosure = new Closure<Void>(this) {
+//            public Void doCall(Object arg) {
+//                if (arg instanceof MathObject) {
+//                    scene.add((MathObject) arg);
+//                } else if (arg instanceof MathObject[]) {
+//                    scene.add((MathObject[]) arg);
+//                } else if (arg instanceof GeogebraLoader) {
+//                    scene.add((GeogebraLoader) arg);
+//                } else {
+//                    throw new IllegalArgumentException("Not supported for add(): " + arg.getClass());
+//                }
+//                return null;
+//            }
+//
+//            public Void doCall(Object... args) {
+//                if (args.length == 0) {
+//                    throw new IllegalArgumentException("Se requiere al menos un argumento");
+//                }
+//                // Si todos los argumentos son MathObject, conviértelos a un array
+//                if (Arrays.stream(args).allMatch(arg -> arg instanceof MathObject)) {
+//                    MathObject[] objs = Arrays.stream(args)
+//                            .map(arg -> (MathObject) arg)
+//                            .toArray(MathObject[]::new);
+//                    scene.add(objs);
+//                } else {
+//                    throw new IllegalArgumentException("Tipos no soportados en múltiples argumentos");
+//                }
+//                return null;
+//            }
+//
+//        };
+//        binding.setVariable("add", addClosure);
+
+
+//        Closure<Void> waitClosure = new Closure<Void>(this) {
+//            public Void doCall(double time) {
+//                scene.waitSeconds(time);
+//                return null;
+//            }
+//        };
+//        binding.setVariable("waitSeconds", waitClosure);
+
+//        Closure<Void> advanceFrameClosure = new Closure<Void>(this) {
+//            public Void doCall() {
+//                scene.advanceFrame();
+//                return null;
+//            }
+//        };
+//        binding.setVariable("advanceFrame", advanceFrameClosure);
+        return binding;
+    }
+
+    public static CompilerConfiguration createCompilerConfiguration(JMathAnimScene scene) {
+        CompilerConfiguration compilerConfig = new CompilerConfiguration();
+        ImportCustomizer imports = new ImportCustomizer();
+
+        imports.addStarImports(
+                "com.jmathanim.Animations",
+                "com.jmathanim.Animations.MathTransform",
+                "com.jmathanim.Animations.Strategies",
+                "com.jmathanim.Animations.Strategies.ShowCreation",
+                "com.jmathanim.Animations.Strategies.Transform",
+                "com.jmathanim.Animations.Strategies.Transform.Optimizers",
+                "com.jmathanim.Cameras",
+                "com.jmathanim.Constructible",
+                "com.jmathanim.Constructible.Conics",
+                "com.jmathanim.Constructible.Lines",
+                "com.jmathanim.Constructible.Others",
+                "com.jmathanim.Constructible.Points",
+                "com.jmathanim.Constructible.Transforms",
+                "com.jmathanim.Enum",
+                "com.jmathanim.MathObjects",
+                "com.jmathanim.MathObjects.Axes",
+                "com.jmathanim.MathObjects.Delimiters",
+                "com.jmathanim.MathObjects.Shapes",
+                "com.jmathanim.MathObjects.Text",
+                "com.jmathanim.MathObjects.Text.TextUpdaters",
+                "com.jmathanim.MathObjects.Tippable",
+                "com.jmathanim.MathObjects.UpdateableObjects",
+                "com.jmathanim.MathObjects.Updaters",
+                "com.jmathanim.Utils",
+                "com.jmathanim.Styling"
+        );
+
+        GroovyUtils.addAllPackagesFromJar(scene,"com.jmathanim", imports);
+
+        compilerConfig.addCompilationCustomizers(imports);
+        return compilerConfig;
+    }
+    public static void addAllPackagesFromJar(JMathAnimScene scene,String basePackage, ImportCustomizer imports) {
+        try {
+            // Obtiene la ubicación del jar actual
+            URL jarUrl = scene.getClass().getProtectionDomain().getCodeSource().getLocation();
+            File jarFile = new File(jarUrl.toURI());
+
+            if (!jarFile.getName().endsWith(".jar")) {
+                JMathAnimScene.logger.warn("Not executing from JAR, using only manual imports");
+                imports.addStarImports(basePackage);
+                return;
+            }
+
+            try (JarFile jar = new JarFile(jarFile)) {
+                Set<String> packages = new HashSet<>();
+                String prefix = basePackage.replace('.', '/') + "/";
+
+                Enumeration<JarEntry> entries = jar.entries();
+                while (entries.hasMoreElements()) {
+                    JarEntry entry = entries.nextElement();
+                    String name = entry.getName();
+                    if (name.endsWith(".class") && name.startsWith(prefix)) {
+                        int lastSlash = name.lastIndexOf('/');
+                        if (lastSlash > 0) {
+                            String pkg = name.substring(0, lastSlash).replace('/', '.');
+                            packages.add(pkg);
+                        }
+                    }
+                }
+
+                for (String pkg : packages) {
+                    imports.addStarImports(pkg);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
 }
