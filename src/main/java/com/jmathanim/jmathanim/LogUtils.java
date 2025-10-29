cf316b9f5efa34a400f870ccf3a2cf98f1ce52e6
DavidGutierrezRubio <davidgutierrezrubio@gmail.com>
2025-10-29 22:50:54 +0100

    -JMathaAnim now tries to find ffmpeg if user didn't set path -Improved logs
    Signed-off-by: DavidGutierrezRubio <davidgutierrezrubio@gmail.com>

--- src/main/java/com/jmathanim/jmathanim/LogUtils.java
+++ src/main/java/com/jmathanim/jmathanim/LogUtils.java
@@ -7,7 +7,7 @@ public class LogUtils {
     public static final String RED = "\u001B[31m";
     public static final String GREEN = "\u001B[32m";
     public static final String YELLOW = "\u001B[33m";
-    public static final String BLUE = "\u001B[34m";
+    public static final String BLUE = "\u001B[94m";
     public static final String PURPLE = "\u001B[35m";
     public static final String CYAN = "\u001B[36m";
     public static final String WHITE = "\u001B[37m";
